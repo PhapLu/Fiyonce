f95644deaa18d13d114e64846fa74435e199c466
Phap Lu <phapluudev2k5@gmail.com>
2024-07-15 21:22:19 +0700

    Update code for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -28,6 +28,7 @@ app.use(cors({
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
     credentials: true,
 }))
+console.log(process.env.ALLOWED_ORIGIN)
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(helmet()) // Using Helmet
--- server/src/configs/socket.config.js
+++ server/src/configs/socket.config.js
@@ -1,9 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import { Server } from 'socket.io'
 
 export default function configureSocket(server) {
     const io = new Server(server, {
         cors: {
-            origin: ["http://localhost:5173"],
+            origin: [process.env.ALLOWED_ORIGIN],
             methods: ["GET", "POST"],
             credentials: true
         }
