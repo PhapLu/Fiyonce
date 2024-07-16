acdd9c569566344644c881e6c6c39ea21a0b1237
Phap Lu <phapluudev2k5@gmail.com>
2024-07-16 11:12:37 +0700

    Roll abck

--- server/src/app.js
+++ server/src/app.js
@@ -1,113 +1,98 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import http from 'http';
-import path from 'path';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import { v4 as uuidv4 } from 'uuid';
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import './db/init.mongodb.js';
-import router from './routes/index.js';
-import myLogger from './loggers/mylogger.log.js';
-import configureSocket from './configs/socket.config.js';
-import SocketServices from './services/socket.service.js';
-import sanitizeInputs from './middlewares/sanitize.middleware.js';
-import { globalLimiter, authLimiter, uploadLimiter, blockChecker } from './configs/rateLimit.config.js';
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express"
+import http from 'http'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import {v4 as uuidv4} from 'uuid'
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
+import './db/init.mongodb.js'
+import router from "./routes/index.js"
+import myLogger from './loggers/mylogger.log.js'
+import configureSocket from './configs/socket.config.js'
+import SocketServices from './services/socket.service.js'
+import sanitizeInputs from './middlewares/sanitize.middleware.js'
+import { globalLimiter, authLimiter, uploadLimiter, blockChecker } from './configs/rateLimit.config.js'
+const app = express()
 
-const app = express();
-
-// Rate Limit
-app.use(blockChecker);
-app.use(globalLimiter);
+//Rate Limit
+app.use(blockChecker)
+app.use(globalLimiter)
 
 // Init middlewares
 app.use(cors({
     origin: [process.env.ALLOWED_ORIGIN],
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
     credentials: true,
-}));
-
-app.use(express.json());
-app.use(morgan('dev'));
-app.use(helmet());
-
+}))
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(helmet()) // Using Helmet
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
     scriptSrc: ["'self'"]
   }
-}));
+}))
+app.use(compression())
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(sanitizeInputs)
 
-app.use(compression());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(sanitizeInputs);
-
-// Advanced Logger
+//Advanced Logger
 app.use((req, res, next) => {
-    const requestId = req.headers['x-request-id'];
-    req.requestId = requestId ? requestId : uuidv4();
+    const requestId = req.headers['x-request-id']
+    req.requestId = requestId ? requestId : uuidv4()
 
     myLogger.log(`input-params ::${req.method}::`, [
         req.path,
         { requestId: req.requestId },
         (req.method === 'POST' || req.method === 'PATCH') ? req.body : req.query
-    ]);
+    ])
 
-    next();
-});
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(path.resolve(), 'public')));
-
-// Serve static files from your build directory if you have a front-end
-app.use(express.static(path.join(path.resolve(), 'client/build')));
+    next()
+})
 
 // Init routes
-app.use('/api', router);
-
-// Error handling for static files
-app.use((req, res, next) => {
-    if (req.method === 'GET' && !req.path.startsWith('/api/')) {
-        res.sendFile(path.join(path.resolve(), 'client/build', 'index.html'));
-    } else {
-        next();
-    }
-});
+app.use('', router)
 
 // Error handling
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-});
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
 
 app.use((error, req, res, next) => {
-    const statusCode = error.status || 500;
-    const resMessage = `${error.status} - ${Date.now() - error.now}ms - Response: ${JSON.stringify(error)}`;
+    const statusCode = error.status || 500
+    const resMessage = `${error.status} - ${Date.now() - error.now}ms - Response: ${JSON.stringify(error)}`
     myLogger.error(resMessage, [
         req.path,
         { requestId: req.requestId},
         { message: error.message}
-    ]);
+    ])
     return res.status(statusCode).json({
         status: 'error',
         code: statusCode,
+        // stack: error.stack,
         message: error.message || 'Internal Server Error'
-    });
-});
+    })
+})
 
 // Create HTTP server
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 // Configure Socket.IO
-configureSocket(server);
+configureSocket(server)
 
-global._io.on('connection', SocketServices.connection);
+global._io.on('connection', SocketServices.connection)
 
-export default app;
+process.on('SIGINT', () => {
+    server.close(() => console.log(`Exit Server Express`))
+})
+
+export default app
