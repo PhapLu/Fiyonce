c9970e185a5ee5415158a52662d6efaab004466e
nhatluudev <nhatluu031103@gmail.com>
2024-08-15 18:28:03 +0700

    commit latest code

--- client/src/dashboard/challengeDashboard/ChallengeDashboard.jsx
+++ client/src/dashboard/challengeDashboard/ChallengeDashboard.jsx
@@ -40,7 +40,7 @@ export default function ChallengeDashboard() {
         }
     };
 
-    const { data: challenges, isError, error, isLoading } = useQuery('fetchChallenges', fetchChallenges);
+    const { data: challenges, isFetchingChallengesError, fetchingChallengesError, isFetchingChallengesLoading } = useQuery('fetchChallenges', fetchChallenges);
     const createChallengeMutation = useMutation(
         (newChallenge) => apiUtils.post("/challenge/createChallenge", newChallenge),
         {
@@ -86,17 +86,130 @@ export default function ChallengeDashboard() {
         }
     );
 
+    if (isFetchingChallengesLoading) return <div>Loading...</div>;
+    if (isFetchingChallengesError) return <div>Error: {fetchingChallengesError.message}</div>;
 
-    if (isLoading) return <div>Loading...</div>;
-    if (isError) return <div>Error: {error.message}</div>;
+
+    const fetchBadges = async () => {
+        try {
+            const response = await apiUtils.get("/badge/readBadges");
+            console.log(response);
+            return response.data.metadata.badges;
+        } catch (error) {
+            return null;
+        }
+    };
+
+    const { data: badges, isFetchingBadgesError, fetchingBadgesError, isFetchingBadgesLoading } = useQuery('fetchBadges', fetchBadges);
+    const createBadgeMutation = useMutation(
+        (newBadge) => apiUtils.post("/badge/createBadge", newBadge),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries('fetchBadges');
+                setOverlayVisible(false);
+                setShowCreateBadge(false);
+                setModalInfo({
+                    status: "success",
+                    message: "Tạo bản tin thành công"
+                })
+            }
+        }
+    );
+
+    const updateBadgeMutation = useMutation(
+        (updatedBadge) => apiUtils.patch(`/badge/updateBadge/${updatedBadge.get("_id")}`, updatedBadge),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries('fetchBadges');
+                setOverlayVisible(false);
+                setShowUpdateBadge(false);
+                setModalInfo({
+                    status: "success",
+                    message: "Cập nhật bản tin thành công"
+                })
+            }
+        }
+    );
+
+    const deleteBadgeMutation = useMutation(
+        (badgeId) => apiUtils.delete(`/badge/deleteBadge/${badgeId}`),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries('fetchBadges');
+                setOverlayVisible(false);
+                setShowDeleteBadge(false);
+                setModalInfo({
+                    status: "success",
+                    message: "Xóa bản tin thành công"
+                })
+            }
+        }
+    );
+
+    if (isFetchingBadgesLoading) return <div>Loading...</div>;
+    if (isFetchingBadgesError) return <div>Error: {fetchingBadgesError.message}</div>;
 
     return (
         <>
             <div className="dashboard-account">
+                {/* Badges */}
                 <section className="section overview">
                     <div className="section-header">
                         <div className="section-header--left">
-                            <h3 className="section-header__title">Bản tin</h3>
+                            <h3 className="section-header__title">Huy hiệu</h3>
+                            <svg onClick={() => { setOverlayVisible(true); setShowCreateChallenge(true) }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 btn add-btn">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                        </div>
+                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>STT</th>
+                                <th>Tên huy hiệu</th>
+                                <th>Mô tả</th>
+                                <th>Điều kiện</th>
+                                <th>Thao tác</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                badges?.length > 0 ? (
+                                    badges.map((badge, index) => (
+                                        <tr key={badge._id}>
+                                            <td><img src={badge.thumbnail} alt="" /></td>
+                                            <td>{index + 1}</td>
+                                            <td><span className='fw-bold'>{badge.title}</span></td>
+                                            <td>{badge.description}</td>
+                                            <td>{badge.criteria?.map((criterion, index) => {
+                                                return (
+                                                    <>
+                                                    </>
+                                                )
+                                            })}</td>
+                                            <td>
+                                                <button className="btn btn-2" onClick={() => { setChallenge(badge); setOverlayVisible(true); setShowUpdateChallenge(true) }}>Chỉnh sửa</button>
+                                                <button className="btn btn-3" onClick={() => { setChallenge(badge); setOverlayVisible(true); setShowDeleteChallenge(true) }}>Xóa</button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan="6" className='text-align-center pt-16 pb-16'>Không có dữ liệu</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </section >
+
+                {/* Challenges */}
+                <section className="section overview">
+                    <div className="section-header">
+                        <div className="section-header--left">
+                            <h3 className="section-header__title">Thử thách</h3>
                             <svg onClick={() => { setOverlayVisible(true); setShowCreateChallenge(true) }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 btn add-btn">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
